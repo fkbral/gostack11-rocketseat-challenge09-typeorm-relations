c2badb33ce1d7fc34c496c9cf2ca1bea2b1bd1ae
Felipe Cabral <felipeo.cabral@hotmail.com>
2020-06-16 21:28:14 -0300

    Create customers and products & create/list orders

--- src/modules/orders/infra/typeorm/entities/Order.ts
+++ src/modules/orders/infra/typeorm/entities/Order.ts
@@ -5,21 +5,33 @@ import {
   UpdateDateColumn,
   OneToOne,
   JoinColumn,
+  JoinTable,
   OneToMany,
 } from 'typeorm';
 
 import Customer from '@modules/customers/infra/typeorm/entities/Customer';
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity('orders')
 class Order {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @OneToOne(() => Customer, { eager: true })
+  @JoinColumn({ name: 'customer_id' })
   customer: Customer;
 
+  @OneToMany(() => OrdersProducts, ordersProducts => ordersProducts.order, {
+    cascade: true,
+    eager: true,
+  })
+  @JoinTable({ name: 'orders_products' })
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
